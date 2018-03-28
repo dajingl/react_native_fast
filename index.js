f92237a8b18e52d5b54442ccee7a7bf89a6f14d4
dj <dajing61@foxmail.com>
2018-03-28 17:36:54 +0800

    添加 Sentry

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-export default class reactNativeFast extends Component {
-    render() {
-        return (
-            <App />
-        );
-    }
+import {Sentry} from  'react-native-sentry';
+if (!__DEV__) {
+    Sentry.config('https://44e2a8612cab4ff1b895f617f70cab38:c0e8e62cf8944f95984ebf9f59cc7d8a@sentry.io/713856').install();
+    global.console = {
+        info: () => {},
+        log: () => {},
+        warn: () => {},
+        error: () => {},
+    };
 }
-AppRegistry.registerComponent('react_native_fast', () => reactNativeFast);
+AppRegistry.registerComponent('react_native_fast', () => App);
